877008c5aef08e1dc40dceec43fb8afc6370940a
Samprit9122000 <samprit.gullu@gmail.com>
2024-08-18 19:44:03 +0530

    asp dot net tut

---
+++ newProject.API/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace newProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet("GetStuents")]
+        public IActionResult GetStuents()
+        {
+            string[] StudentData = new string[] { "John", "Json", "Harry" };
+            return Ok(StudentData);
+        }
+
+        [HttpGet("GetOne")]
+        public IActionResult GetOne()
+        {
+            string[] StudentData = new string[] { "John" };
+            return Ok(StudentData);
+        }
+
+    }
+}
